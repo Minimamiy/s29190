38ae832a40638286734bda0b3f127e6586c9dbd2
Mina <s29180@pjwstk.edu.pl>
2023-03-18 14:46:30 -0400

    another one + one

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void printSomething(){
         System.out.println("Hello Mars");
         System.out.println("Another one");
+        System.out.println("Another secoond");
     }
 }
